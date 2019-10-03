370a16ce5748ccd040c6f1c81b4398cb29b76d24
ClaudiuBu <37150852+ClaudiuBu@users.noreply.github.com>
2019-10-03 11:33:10 +0300

    initial theme files

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+
+    <title><?php bloginfo('name'); ?></title>
+</head>
+<body>
+    
+
+<?php wp_footer() ?>
+</body>
+</html>
